d9a55785d624353505d1095a455c049ca0b526d8
mengdanzhu <42902261+mengdanzhu@users.noreply.github.com>
2018-11-17 19:21:32 -0500

    Update project1.sql

--- project1/project1.sql
+++ project1/project1.sql
@@ -1,11 +1,7 @@
+--question1
 SELECT * into mzhu.demo from Demographics
-select top 10 * from mzhu.demo
-select * from mzhu.demo
-select * from demographics
-
 Alter table mzhu.demo
 DROP COLUMN gh1, gh2, pf02,gh3,gh4,gh5,test
---question1
 sp_RENAME 'mzhu.demo.tri_age', 'age', 'COLUMN'
 sp_RENAME 'mzhu.demo.gendercode', 'Gender', 'COLUMN'
 sp_RENAME 'mzhu.demo.contactid', 'id', 'COLUMN'
@@ -15,6 +11,8 @@ sp_RENAME 'mzhu.demo.tri_enrollmentcompletedate', 'Completedate', 'COLUMN'
 
 Alter table mzhu.demo
 add timeforcompletion as DATEDIFF(day, try_convert(date, EmailSentdate), try_convert(date,Completedate)) 
+                                                                                     
+select top 10 * from mzhu.demo                                                                                    
 --question2
 Alter table mzhu.demo
 ADD EnrollmentStatus NVARCHAR(50) 
@@ -42,6 +40,7 @@ WHERE tri_imaginecareenrollmentstatus=167410000
 UPDATE mzhu.demo
 SET EnrollmentStatus='Second email sent'
 WHERE tri_imaginecareenrollmentstatus=167410006
+select top 10 * from mzhu.demo                                                                                     
 --question3
 Alter table mzhu.demo
 ADD SEX NVARCHAR(50) 
@@ -57,6 +56,7 @@ WHERE Gender='167410000'
 UPDATE mzhu.demo
 SET SEX='Unknown'
 WHERE Gender='NULL'
+select top 10 * from mzhu.demo                                                                                     
 --question4
 Alter table mzhu.demo
 ADD AgeGroup NVARCHAR(50)
@@ -72,3 +72,4 @@ WHERE age>=51 AND age<=75
 UPDATE mzhu.demo
 SET AgeGroup='76-100'
 WHERE age>=76 AND age<=100
+select top 10 * from mzhu.demo
